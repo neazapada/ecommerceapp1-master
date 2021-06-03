c8269bbd883989378ca61b6a3fffeb1dceb9262b
Neazapada <paraschivgabriel89@gmail.com>
2021-06-03 15:23:47 +0300

    Product details

---
+++ angular-ecommerceapp/src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {ActivatedRoute} from "@angular/router";
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  public product;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => this.productDetailPage());
+  }
+
+  productDetailPage() {
+    const hasProductId = this.route.snapshot.paramMap.has('id');
+    if(hasProductId) {
+      this.productService.listProductDetailPage(+this.route.snapshot.paramMap.get('id')).subscribe(
+        data => {
+          console.log(data);
+          this.product = data;
+        }
+      );
+    }
+  }
+}
